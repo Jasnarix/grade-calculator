01a02855c665c5aacf722eaf3e21edf43f8cc34d
Jason Sundra <jasonsundra@gmail.com>
2025-12-10 21:25:03 +0800

    changed code for edge cases

--- StudentGrades.java
+++ StudentGrades.java
@@ -1,8 +1,11 @@
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class StudentGrades {
     private int number_of_grades;
+    private float grade;
+    private float weight;
     private ArrayList<Result> results;
     private float gradeCalc;
     private float weightCalc;
@@ -12,6 +15,8 @@ public class StudentGrades {
     private int loopCounter = 1;
     private ArrayList<Float> grade_arrayList = new ArrayList<>();
     private ArrayList<Float> weight_arrayList = new ArrayList<>();
+    private boolean valid_grade = false;
+    private boolean valid_weight = false;
 
 
     public StudentGrades(){
@@ -27,24 +32,31 @@ public class StudentGrades {
 
     public void addResults(){
         for(int i = 0; i < this.number_of_grades; i++){
+
             Scanner grade_input = new Scanner(System.in);
-            System.out.print("Enter grade for Assignment " +loopCounter +": ");
-            if(grade_input.hasNextFloat()){
-                float grade = grade_input.nextFloat();
-            } else {
-                System.out.println("This is not a valid input.");
-            }
-
             Scanner weight_input = new Scanner(System.in);
-            System.out.print("Enter Weight for Assignment " + loopCounter + ": " );
-            if(weight_input.hasNextFloat()){
-                float weight = grade_input.nextFloat();
-            } else {
-                System.out.println("This is not a valid input.");
-            }
-            
-            float weight = weight_input.nextFloat();
 
+            while(!valid){
+                try{
+                    System.out.print("Enter grade for Assignment " +loopCounter +": ");
+                    float grade = grade_input.nextFloat();
+                    valid_grade = true;
+                } catch (InputMismatchException e){
+                    System.out.print("That is an invalid input. Please enter a number.");
+                    grade_input.nextLine();
+                }
+            }
+
+            while(!valid_weight){
+                try{
+                System.out.print("Enter Weight for Assignment " + loopCounter + ": " );
+                float weight = grade_input.nextFloat();
+                valid_weight = true;
+                } catch (InputMismatchException e){
+                    System.out.print("That is an invalid input. Please enter a number.");
+                    weight_input.nextLine();
+                }
+            }
             Result result_obj = new Result(grade, weight);
             results.add(result_obj);
             loopCounter++;
